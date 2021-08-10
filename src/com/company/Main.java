97713e1756c89c9bbeb25855631c399b72f4b600
jonaskaufmann <jonas.kaufmann@sunrise.ch>
2021-08-10 10:50:30 +0200

    start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -60,9 +60,57 @@ public static String[][] spielFeld = new String[3][3];
 
     }
     public static Boolean ueberpruefung(Boolean spielen) {
-        if (spielFeld[0][0].equalsIgnoreCase(spielFeld[0][1]) && spielFeld[0][0].equalsIgnoreCase(spielFeld[0][2]) ) {
-            spielen = false;
+        {
+            char Winner = ' ';
+
+            // Check if X wins
+            if (posn[1] == 'X' && posn[2] == 'X' && posn[3] == 'X') Winner = 'X';
+            if (posn[4] == 'X' && posn[5] == 'X' && posn[6] == 'X') Winner = 'X';
+            if (posn[7] == 'X' && posn[8] == 'X' && posn[9] == 'X') Winner = 'X';
+            if (posn[1] == 'X' && posn[4] == 'X' && posn[7] == 'X') Winner = 'X';
+            if (posn[2] == 'X' && posn[5] == 'X' && posn[8] == 'X') Winner = 'X';
+            if (posn[3] == 'X' && posn[6] == 'X' && posn[9] == 'X') Winner = 'X';
+            if (posn[1] == 'X' && posn[5] == 'X' && posn[9] == 'X') Winner = 'X';
+            if (posn[3] == 'X' && posn[5] == 'X' && posn[7] == 'X') Winner = 'X';
+            if (Winner == 'X' )
+            {System.out.println("Player1 wins the game." );
+                return Winner;
+            }
+
+            // Check if O wins
+            if (posn[1] == 'O' && posn[2] == 'O' && posn[3] == 'O') Winner = 'O';
+            if (posn[4] == 'O' && posn[5] == 'O' && posn[6] == 'O') Winner = 'O';
+            if (posn[7] == 'O' && posn[8] == 'O' && posn[9] == 'O') Winner = 'O';
+            if (posn[1] == 'O' && posn[4] == 'O' && posn[7] == 'O') Winner = 'O';
+            if (posn[2] == 'O' && posn[5] == 'O' && posn[8] == 'O') Winner = 'O';
+            if (posn[3] == 'O' && posn[6] == 'O' && posn[9] == 'O') Winner = 'O';
+            if (posn[1] == 'O' && posn[5] == 'O' && posn[9] == 'O') Winner = 'O';
+            if (posn[3] == 'O' && posn[5] == 'O' && posn[7] == 'O') Winner = 'O';
+            if (Winner == 'O' )
+            {
+                System.out.println( "Player2 wins the game." );
+                return Winner; }
+
+            // check for Tie
+            for(int i=1;i<10;i++)
+            {
+                if(posn[i]=='X' || posn[i]=='O')
+                {
+                    if(i==9)
+                    {
+                        char Draw='D';
+                        System.out.println(" Game is stalemate ");
+                        return Draw;
+                    }
+                    continue;
+                }
+                else
+                    break;
+
+            }
+
+            return Winner;
         }
-        return spielen;
+
     }
 }

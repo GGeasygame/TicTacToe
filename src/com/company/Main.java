7545380326782108366f442d08c45bcc1388471e
jonaskaufmann <jonas.kaufmann@sunrise.ch>
2021-08-10 08:34:06 +0200

    start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         int[][] spielFeld = new int[3][3];
+        Scanner scanner = new Scanner(System.in);
+
+        Boolean spielen = true;
+        while (spielen) {
+            spieler1(spielFeld, scanner);
+
+
+
+
+        }
+    }
+    public static int spieler1(int[][] spielFeld, Scanner scanner){
+
+
+
+
 
     }
 }

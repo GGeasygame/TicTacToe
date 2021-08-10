adc378cf7b938d0db64f3067c5c32e7c40e0efc1
jonaskaufmann <jonas.kaufmann@sunrise.ch>
2021-08-10 08:52:16 +0200

    start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,35 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.jar.JarOutputStream;
 
 public class Main {
-
+public static int var1 = 1;
     public static void main(String[] args) {
-        int[][] spielFeld = new int[3][3];
+        int[] spielFeld = new int[9];
         Scanner scanner = new Scanner(System.in);
 
         Boolean spielen = true;
         while (spielen) {
+            System.out.println( var1 + "  |  " + var2  + "  |  " + var3);
+            System.out.println("---------------------------------------");
+            System.out.println( var4 + "  |  " + var5  + "  |  " + var6);
+            System.out.println("---------------------------------------");
+            System.out.println( var7 + "  |  " + var8  + "  |  " + var9);
             spieler1(spielFeld, scanner);
+
 
 
 
 
         }
     }
-    public static int spieler1(int[][] spielFeld, Scanner scanner){
+    public static int spieler1(int[] spielFeld, Scanner scanner){
+
+        System.out.println("Wählen Sie das Eingabefeld aus.");
+        int eingabe1 = scanner.nextInt();
+        spielFeld[eingabe1]= 1;
+
 
 
 

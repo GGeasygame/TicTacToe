b67c006d3abd8fa7ff46f7f5dc60173db5ebaadc
SandroHartmann <88325031+SandroHartmann@users.noreply.github.com>
2021-08-10 10:45:42 +0200

    Hallo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -31,17 +31,40 @@ public static String[][] spielFeld = new String[3][3];
                 System.out.println("    -----------------");
                 System.out.println("3     " + spielFeld[0][2] + "  |  " + spielFeld[1][2] + "  |  " + spielFeld[2][2]);
             }
-            spieler1(scanner);
-
-
-
-
-
+            if (spielen) {
+                spieler2(scanner);
+                System.out.println("      A     B     C");
+                System.out.println("1     " + spielFeld[0][0] + "  |  " + spielFeld[1][0] + "  |  " + spielFeld[2][0]);
+                System.out.println("    -----------------");
+                System.out.println("2     " + spielFeld[0][1] + "  |  " + spielFeld[1][1] + "  |  " + spielFeld[2][1]);
+                System.out.println("    -----------------");
+                System.out.println("3     " + spielFeld[0][2] + "  |  " + spielFeld[1][2] + "  |  " + spielFeld[2][2]);
+            }
         }
     }
     public static void spieler1(Scanner scanner){
 
-        System.out.println("Wählen Sie Ihre Spalte aus.");
+        System.out.println("Spieler 1: Wählen Sie Ihre Spalte aus.");
+        String eingabe1 = scanner.next();
+        System.out.println("Wählen Sie Ihre Zeile aus.");
+        int eingabe2 = scanner.nextInt();
+        int eingabe1Int;
+        if (eingabe1.equalsIgnoreCase("a")) {
+            eingabe1Int = 0;
+        } else if (eingabe1.equalsIgnoreCase("b")) {
+            eingabe1Int = 1;
+        } else {
+            eingabe1Int = 2;
+        }
+
+
+        spielFeld[eingabe1Int][eingabe2 - 1]= "0";
+        System.out.println(eingabe2 - 1);
+
+    }
+    public static void spieler2(Scanner scanner){
+
+        System.out.println("Spieler 2: Wählen Sie Ihre Spalte aus.");
         String eingabe1 = scanner.next();
         System.out.println("Wählen Sie Ihre Zeile aus.");
         int eingabe2 = scanner.nextInt();
@@ -60,9 +83,8 @@ public static String[][] spielFeld = new String[3][3];
 
     }
     public static Boolean ueberpruefung(Boolean spielen) {
-        if (spielFeld[0][0].equalsIgnoreCase(spielFeld[0][1]) && spielFeld[0][0].equalsIgnoreCase(spielFeld[0][2]) ) {
-            spielen = false;
-        }
+        if ( )
+
         return spielen;
     }
 }
